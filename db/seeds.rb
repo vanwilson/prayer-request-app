40ec8921a509671136d0f990112356c032144aac
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-01 15:16:21 -0500

    add prayer model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+prayer = Prayer.new(title: "Medical Tests", body: "I'm having CT scan to see why I'm haivng headaches.")
+prayer.save
