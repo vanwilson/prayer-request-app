53d1e785707d2b867ebdb0ac0f663f89991d7b6f
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-12-20 14:25:12 -0600

    updates user controller to take out phone_number and email attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,6 @@ class UsersController < ApplicationController
     @user = User.create(
       first_name: params[:first_name],
       last_name: params[:last_name],
-      email: params[:email],
-      phone_number: params[:phone_number],
     )
     render :show
   end
