48ba3e11a2a00d8764cdf9bece0fed97fd23608d
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-12-20 16:00:56 -0600

    adds password digest to user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :prayers
+
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,8 +11,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_difference "User.count", 1 do
-      post "/users.json", params: { first_name: "lake", last_name: "geneva" }
-      assert_response 200
+      post "/users.json", params: { first_name: "Test", last_name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
+      assert_response 201
     end
   end
 
