5118860fc36d06aced2bd4688a31e104f985d004
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-12-21 15:58:51 -0600

    adds rack cors gem

---
+++ config/initializers/cors.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "*"
+    origins "localhost:5173"
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
+  end
+end
