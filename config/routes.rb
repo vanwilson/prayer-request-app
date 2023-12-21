3054444720f20402a431d918c4daa644941d3fc2
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-12-21 11:27:30 -0600

    add sessions create action (login)

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get "/users/:id" => "users#show"
   patch "/users/:id" => "users#update"
   delete "/users/:id" => "users#destroy"
+
+  post "/sessions" => "sessions#create"
 end
