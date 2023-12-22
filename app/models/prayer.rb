25c81c9ec3c6e50c423a19dd743dfe38334d8c68
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-12-22 13:11:20 -0600

    shortens validations in prayer model

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -1,7 +1,4 @@
 class Prayer < ApplicationRecord
   belongs_to :user
-  validates :pray_for, presence: true
-  validates :title, presence: true
-  validates :prayer_type, presence: true
-  validates :body, presence: true
+  validates :pray_for, :title, :prayer_type, :body, presence: true
 end
